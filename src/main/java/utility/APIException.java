03de1eb066ca1d21cfd2bc24befc9bf4b5bc3811
dennismozart1994 <dennis.mozart@gmail.com>
2018-08-05 02:57:41 -0300

    Gurock's API
    API to Update TestRail TestCases Results

---
+++ src/main/java/utility/APIException.java
@@ -0,0 +1,21 @@
+/**
+ * TestRail API binding for Java (API v2, available since TestRail 3.0)
+ *
+ * Learn more:
+ *
+ * http://docs.gurock.com/testrail-api2/start
+ * http://docs.gurock.com/testrail-api2/accessing
+ *
+ * Copyright Gurock Software GmbH. See license.md for details.
+ */
+
+package utility;
+ 
+@SuppressWarnings("serial")
+public class APIException extends Exception
+{
+	public APIException(String message)
+	{
+		super(message);
+	}
+}
